b1be9b2bff2669e346f024683f30f849099681de
unknown <phamhoaiphatcd.12t2@gmail.com>
2021-08-20 16:19:08 +0700

    BaiTapDatCho

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template:``
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularFrontend66';
+}
